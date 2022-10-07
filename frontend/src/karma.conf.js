9593bbbc43938490268f812a5d44c800ce57771f
jonathanmetzman <31354670+jonathanmetzman@users.noreply.github.com>
2022-10-07 13:15:07 -0400

    Update karma.conf.js

--- frontend/src/karma.conf.js
+++ frontend/src/karma.conf.js
@@ -6,6 +6,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// test
 module.exports = function (config) {
   config.set({
     basePath: '',
